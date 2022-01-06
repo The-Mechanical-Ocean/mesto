a11bd31f993197a6969023d8b61ffdc6976d3873
The-Mechanical-Ocean <alex.kudrin@bk.ru>
2022-01-06 14:44:17 +0300

    "feat: сделать функции открыти popup add and edit"

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,36 @@
-const popupOpenButton = document.querySelector('.profile__button-edit');
-const popupCloseButton = document.querySelector('.popup__button-close');
-const popup = document.querySelector('.popup');
-const popupForm = document.querySelector('.popup__container');
+const popupOpenButtonEdit = document.querySelector('.profile__button-edit');
+const formProfile = document.querySelector('.profile');
+const popupOpenButtonAdd =  formProfile.querySelector('.profile__button-add');
+const popupAdd = document.querySelector('.popup_type_add-card');
+const popupCloseButtonEdit = document.querySelector('.popup__button-close_type_edit');
+const popupCloseButtonAdd =  popupAdd.querySelector('.popup__button-close_type_add-card');
+const popupEdit = document.querySelector('.popup_type_edit');
+
+const popupFormEdit = document.querySelector('.popup__container_type_edit');
+const popupFormAdd = document.querySelector('.popup__container_type_add-card');
 const popupUserName = document.querySelector('.popup__input_type_name');
 const popupUserDescription = document.querySelector('.popup__input_type_job');
+const popupNameImg = document.querySelector('.popup__input_type_name-img'); //?
+const popupNameLink = document.querySelector('.popup__input_type_link');  //?
 const profileUserName = document.querySelector('.profile__name');
 const profileDescription = document.querySelector('.profile__description');
 
-function openPopup() {
+console.log(popupOpenButtonAdd);
+
+function openPopupEdit() {
   popupUserName.value = profileUserName.textContent;
   popupUserDescription.value = profileDescription.textContent;
-  popup.classList.add('popup_opened');
+  popupEdit.classList.add('popup_opened');
 }
 
-function closedPopup() {
-  popup.classList.remove('popup_opened');
+function closedPopupEdit() {
+  popupEdit.classList.remove('popup_opened');
 }
 
-popupOpenButton.addEventListener('click', openPopup);
+function togglePopup() {
+  popupAdd.classList.toggle('popup_opened');
+}
+
 
 function formSubmitHandler (evt) {
   evt.preventDefault();
@@ -25,9 +38,54 @@ function formSubmitHandler (evt) {
     profileUserName.textContent = popupUserName.value;
     profileDescription.textContent = popupUserDescription.value;
 
-    closedPopup();
-
+    closedPopupEdit();
 }
 
-popupCloseButton.addEventListener('click', closedPopup);
-popupForm.addEventListener('submit', formSubmitHandler);
+popupOpenButtonEdit.addEventListener('click', openPopupEdit);
+popupCloseButtonEdit.addEventListener('click', closedPopupEdit);
+popupFormEdit.addEventListener('submit', formSubmitHandler);
+
+popupOpenButtonAdd.addEventListener('click', togglePopup);
+popupCloseButtonAdd.addEventListener('click', togglePopup);
+
+
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+  const list = document.querySelector('.cards__items')
+  const cardTemplate = document.querySelector('.cards-template').content
+
+initialCards.forEach(function(cardData){
+  const cardElement = cardTemplate.cloneNode(true)
+  const cardImg = cardElement.querySelector('.cards__img')
+  const cardText = cardElement.querySelector('.cards__text')
+
+  cardText.textContent = cardData.name
+  cardImg.src = cardData.link
+  list.prepend(cardElement)
+})
